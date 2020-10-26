4f3f350cca28fe7a27519e37e97b9f28c6b7bb6d
wilmer pachon <wrpachonl@gmail.com>
2020-10-26 11:11:14 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import os,sys
 workingPath = os.getcwd()
 sys.path.append(os.path.join(workingPath, "src"))
 from src.game import Game
-from src.camera import Camera
 
 if __name__ == "__main__":
     Game.getGame()
